6e0717f0606925d85942e6d2ef3f4130cd754af0
TasifTarikul <tasiftarikul@gmail.com>
2024-01-30 20:25:59 +0600

    added comments and renamed variables for clarity

--- project_root/product/api/admin/serializers.py
+++ project_root/product/api/admin/serializers.py
@@ -27,40 +27,42 @@ class AddProductSerializer(serializers.ModelSerializer):
         model = Product
         fields = ('title','category','product_variants','product_images')
 
-
+    # takes the list of attributes of a product variant and create product variant object
     def create_attribute_obj(self,product_variant_obj, attribute_list):
-        for pv_attrs in attribute_list:
+        for product_variant_attr in attribute_list:
             attr_obj = Attribute()
-            attr_obj.title = pv_attrs['title']
-            attr_obj.value = pv_attrs['value']
-            if 'image' in pv_attrs.keys():
-                attr_obj.image = pv_attrs['image']
+            attr_obj.title = product_variant_attr['title']
+            attr_obj.value = product_variant_attr['value']
+            if 'image' in product_variant_attr.keys():
+                attr_obj.image = product_variant_attr['image']
             attr_obj.product_variant = product_variant_obj
             attr_obj.save()
         return
 
+    # takes the list of product variants of a product and create product variant object
     def create_product_variant_obj(self, product_obj, product_variant_list):
-        for pv in product_variant_list:
-            pv_obj = ProductVariant()
-            pv_obj.price = pv['price']
-            pv_obj.qty = pv['qty']
-            pv_obj.sku = pv['sku']
-            pv_obj.description = pv['description']
-            pv_obj.product = product_obj
-            pv_obj.save()
-            self.create_attribute_obj(pv_obj, pv['attributes'])
+        for product_variant in product_variant_list:
+            product_variant_obj = ProductVariant()
+            product_variant_obj.price = product_variant['price']
+            product_variant_obj.qty = product_variant['qty']
+            product_variant_obj.sku = product_variant['sku']
+            product_variant_obj.description = product_variant['description']
+            product_variant_obj.product = product_obj
+            product_variant_obj.save()
+            self.create_attribute_obj(product_variant_obj, product_variant['attributes'])
         return
-
-    def create_product_image_obj(self, product_obj, p_image_list):
-        for p_image in p_image_list:
-            p_image_obj = ProductImage()
-            p_image_obj.image = p_image['image']
-            if 'display_picture' in p_image.keys():
-                p_image_obj.display_picture = p_image['display_picture']
-            p_image_obj.product = product_obj
-            p_image_obj.save()
+    # takes the list of product images of a product and create product image object
+    def create_product_image_obj(self, product_obj, product_image_list):
+        for product_image in product_image_list:
+            product_image_obj = ProductImage()
+            product_image_obj.image = product_image['image']
+            if 'display_picture' in product_image.keys():
+                product_image_obj.display_picture = product_image['display_picture']
+            product_image_obj.product = product_obj
+            product_image_obj.save()
         return
             
+    # Creates a product object
     def create_product_obj(self, title, category, pv_list, p_image_list):
         product_obj = Product()
         product_obj.title = title
@@ -70,7 +72,7 @@ class AddProductSerializer(serializers.ModelSerializer):
         self.create_product_image_obj(product_obj, p_image_list)
         return product_obj
     
-
+    # override create method to save objects of multiple Models
     def create(self, validated_data):
         product_obj = self.create_product_obj(validated_data['title'], 
                                               validated_data['category'], 
