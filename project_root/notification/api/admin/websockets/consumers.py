6a71ab7174148384673e1d43b1225a07122574c1
TasifTarikul <tasiftarikul@gmail.com>
2024-02-10 21:42:10 +0600

    notification code cleaned

--- project_root/notification/api/admin/websockets/consumers.py
+++ project_root/notification/api/admin/websockets/consumers.py
@@ -5,11 +5,8 @@ from channels.generic.websocket import WebsocketConsumer
 class NotificationConsumer(WebsocketConsumer):
 
     def connect(self):
-        print('connect')
         self.room_name = self.scope['url_route']['kwargs']['room_name']
         self.room_group_name = 'notification_%s' % self.room_name
-        print(self.room_group_name)
-        print(self.channel_layer)
         # Join room group
         async_to_sync(self.channel_layer.group_add)(
             self.room_group_name,
@@ -18,7 +15,6 @@ class NotificationConsumer(WebsocketConsumer):
         self.accept()
 
     def disconnect(self, close_code):
-        print('disconnect')
         # Leave room group
         async_to_sync(self.channel_layer.group_discard)(
             self.room_group_name,
@@ -26,9 +22,7 @@ class NotificationConsumer(WebsocketConsumer):
         )
 
     def receive(self, text_data):
-        print('receive')
         # Receive message from WebSocket
-        print(text_data)
         # Send message to room group
         async_to_sync(self.channel_layer.group_send)(
             self.room_group_name,
@@ -39,7 +33,6 @@ class NotificationConsumer(WebsocketConsumer):
         )
 
     def notify(self, event):
-        print('tasif consumer')
         # Receive message from room group
         text = event['message']
         # Send message to WebSocket
