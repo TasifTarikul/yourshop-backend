4bc56622016e52881f7e0e206c7d77d98580b453
TasifTarikul <tasiftarikul@gmail.com>
2024-02-18 11:31:23 +0600

    added url paths

--- project_root/product/api/admin/urls.py
+++ project_root/product/api/admin/urls.py
@@ -1,9 +1,9 @@
 from django.urls import path, include
 
 urlpatterns = [
-    path('', include('project_root.product.api.admin.product.urls',)),
-    path('', include('project_root.product.api.admin.product_variant.urls',)),
-    path('', include('project_root.product.api.admin.product_image.urls',)),
-    path('', include('project_root.product.api.admin.attribute.urls',))
+    path('product/', include('project_root.product.api.admin.product.urls',)),
+    path('product-variant/', include('project_root.product.api.admin.product_variant.urls',)),
+    path('product-image/', include('project_root.product.api.admin.product_image.urls',)),
+    path('attribute/', include('project_root.product.api.admin.attribute.urls',))
 ]
 
