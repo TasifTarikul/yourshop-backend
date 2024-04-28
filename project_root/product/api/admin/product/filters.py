1f9b277d86c2d79542dc81ee4785148fdd247317
TasifTarikul <tasiftarikul@gmail.com>
2024-04-28 07:33:45 +0600

    category_attribute_value filter option added in porduct_api

--- project_root/product/api/admin/product/filters.py
+++ project_root/product/api/admin/product/filters.py
@@ -3,6 +3,7 @@ from django_filters import rest_framework as filters
 from project_root.product.models import Product
 
 class ProductFilter(filters.FilterSet):
+    category_attribute_value = filters.NumberFilter(field_name='product_variants__category_attribute_value')
     class Meta:
         model = Product
-        fields = ['created_at', 'category']
+        fields = ['created_at', 'category', 'category_attribute_value', 'brand']
